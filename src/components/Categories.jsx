de4ce27b1fa93c8f6538981d2207d368b8a6cd82
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2023-01-21 14:48:17 +0200

    #2

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+	return (
+		<div class="categories">
+			<ul>
+				<li class="active">All</li>
+				<li>Meal</li>
+				<li>Vegeterian</li>
+				<li>Grill</li>
+				<li>Chilly</li>
+				<li>Closed</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Categories;
