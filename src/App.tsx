603cb88f4fe8f06ca959f411edfff0e0ed3a289a
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2023-01-26 20:34:22 +0200

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import './scss/app.scss';
@@ -6,17 +6,32 @@ import './scss/app.scss';
 import MainLayout from './layouts/MainLayout';
 
 import Home from './pages/Home';
-import Cart from './pages/Cart';
-import FullPizza from './pages/FullPizza';
 import NotFound from './components/NotFoundBlock';
+
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart"*/ './pages/Cart'));
+const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPiza"*/ './pages/FullPizza'));
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<MainLayout />}>
 				<Route path="" element={<Home />} />
-				<Route path="cart" element={<Cart />} />
-				<Route path="pizza/:id" element={<FullPizza />} />
+				<Route
+					path="cart"
+					element={
+						<Suspense fallback={<div>Loading cart...</div>}>
+							<Cart />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="pizza/:id"
+					element={
+						<Suspense fallback={<div>Loading cart...</div>}>
+							<FullPizza />
+						</Suspense>
+					}
+				/>
 				<Route path="*" element={<NotFound />} />
 			</Route>
 		</Routes>
